9c0e4c65a85147e1bccfc6435627e0595588cf3f
juneh2633 <juneh2633@gmail.com>
2025-01-18 13:50:23 +0900

    feat: rank code

---
+++ src/common/lib/chart-type-code.ts
@@ -0,0 +1,11 @@
+export enum ChartTypeCode {
+  novice = 1,
+  advanced = 2,
+  exhaust = 3,
+  maximum = 4,
+  infinite = 5,
+  gravity = 6,
+  heavenly = 7,
+  vivid = 8,
+  exceed = 9,
+}
---
+++ src/common/lib/clear-rank-code.ts
@@ -0,0 +1,7 @@
+export enum ClearRankCode {
+  per = 0,
+  uc = 1,
+  comp_ex = 2,
+  comp = 3,
+  play = 4,
+}
